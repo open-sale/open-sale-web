55d773136bb8dad9533c7b4915d5be7fbac9d361
muathye <muath.ye@gmail.com>
2022-01-13 23:43:53 +0300

    Add apiResource for category and product @ api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('/category', 'CategoryController')->only(['index', 'show']);
+Route::apiResource('/product', 'ProductController')->only(['index', 'show']);
