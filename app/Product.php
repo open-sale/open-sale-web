f31af946c304610b8bf2756c8f7e30c4109cfd10
muathye <muath.ye@gmail.com>
2022-01-13 23:00:19 +0300

    Add new attributes for Product Model

--- app/Product.php
+++ app/Product.php
@@ -9,6 +9,49 @@ class Product extends Model
 {
     use ModelHelper;
 
+    /**
+     * This path is inside public/uploads/ folder.
+     */
+    public const IMAGE_PATH = 'products/';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'image',
+        'price',
+        'size',
+        'colors',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'colors' => 'array',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'image_path',
+    ];
+
+    public function getImagePathAttribute()
+    {
+        return uploads_path(self::IMAGE_PATH . $this->image);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
