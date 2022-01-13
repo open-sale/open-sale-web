f87a1e7de20dc1e180d7b3e960a12920a9aa4318
muathye <muath.ye@gmail.com>
2022-01-13 23:42:43 +0300

    Update helper.php

--- resources/helpers.php
+++ resources/helpers.php
@@ -1,6 +1,26 @@
 <?php
 
-function uploads_path($path = '')
-{
-    return asset('uploads/' . $path);
+
+if (!function_exists('uploads_path')) {
+    /**
+     * Generate an asset path for the uploads files.
+     *
+     * @return string
+     */
+    function uploads_path($path = '')
+    {
+        return asset('uploads/' . $path);
+    }
+}
+
+if (!function_exists('json_success')) {
+    /**
+     * Get success json response
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function json_success($data = [])
+    {
+        return response()->json(['success' => true, 'data' => $data]);
+    }
 }
