53e92425c730cf9f27febb52be06886fdb1fb9f5
muathye <muath.ye@gmail.com>
2022-01-13 23:00:01 +0300

    Add missed relation

--- app/Category.php
+++ app/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use ModelHelper;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
