d347be772c9a8ead944c4859dd8d005769e097b5
muathye <muath.ye@gmail.com>
2022-01-16 12:31:23 +0300

    Update category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CategoryController extends Controller
 {
@@ -17,6 +19,20 @@ class CategoryController extends Controller
     }
 
     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // TODO: validation [required|string]
+        $category = Category::create($request->only('name'));
+
+        return json_success($category, Response::HTTP_CREATED);
+    }
+
+    /**
      * Display the specified resource.
      *
      * @param  \App\Category  $category
@@ -25,5 +41,34 @@ class CategoryController extends Controller
     public function show(Category $category)
     {
         return json_success($category->products);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        // TODO: validation [required|string]
+        $category = $category->update($request->only('name'));
+
+        return json_success($category, Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category = $category->delete();
+
+        return json_success($category, Response::HTTP_OK);
     }
 }
