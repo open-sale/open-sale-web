6cae10608320d9b35e1dbc6af611ff36d748bff8
muathye <muath.ye@gmail.com>
2022-01-16 12:28:49 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('/category', 'CategoryController')->only(['index', 'show']);
+Route::apiResource('/category', 'CategoryController');//->only(['index', 'show']);
 Route::apiResource('/product', 'ProductController')->only(['index', 'show']);
