ad1cfe2a3a7b3ab48e201a9502ca74231bd6e1a4
muathye <muath.ye@gmail.com>
2022-01-30 00:07:11 +0300

    Update api_v1.php

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->group(function () {
     Route::post('/register', 'register');
-    Route::get('/login','visitor')->name('login'); // redirect to if not authenticated
+    Route::get('/visitor', 'visitor'); // redirect to if not authenticated
     Route::post('/login', 'login');
 });
 
