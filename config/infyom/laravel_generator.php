9e322314e82a13a7fb616020298ca19bb9b655f9
muathye <muath.ye@gmail.com>
2022-01-29 11:02:29 +0300

    Update laravel_generator

--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -39,7 +39,7 @@ return [
 
         'tests'             => base_path('tests/'),
 
-        'views'             => resource_path('views/admin'),
+        'views'             => resource_path('views/admin/'),
 
         'schema_files'      => resource_path('model_schemas/'),
 
