3abdd61fb1af693d94ea7542bb5186d3f8b1a46b
muathye <muath.ye@gmail.com>
2022-01-29 09:41:10 +0300

    Add requests

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -2,17 +2,46 @@
 
 namespace App\Http\Requests;
 
+use App\Exceptions\ApiException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Http\FormRequest;
 
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 
 class ApiRequest extends FormRequest
 {
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
     protected function failedValidation(Validator $validator)
     {
-        throw new HttpResponseException(response()->json($validator->errors(), 422));
+        throw new HttpResponseException(
+            response()->json($validator->errors(), Response::HTTP_FORBIDDEN)
+        );
+    }
+
+    /**
+     * Handle a failed authorization attempt.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    protected function failedAuthorization()
+    {
+        throw new ApiException(
+            'This action is unauthorized.',
+            'Please, Check your permissions.',
+            Response::HTTP_UNAUTHORIZED
+        );
     }
 
     /**
--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -1,7 +1,8 @@
 <?php
-namespace App\Http\Requests ;
 
-class UserRegisterRequest  extends ApiRequest
+namespace App\Http\Requests;
+
+class UserRegisterRequest extends ApiRequest
 {
     public function authorize()
     {
@@ -11,18 +12,9 @@ class UserRegisterRequest  extends ApiRequest
     public function rules()
     {
         return [
-            'name'=>'required|string',
-            'email'=>'required|string|unique:users,email',
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
             // 'password'=>'required|string|confirmed',
         ];
     }
-
-
-
 }
-
-
-
-
-
-?>
