427a6f5a498398dd5798dd0182232c68044d5bed
muathye <muath.ye@gmail.com>
2022-01-29 22:18:12 +0300

    Add icons to menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,24 +1,24 @@
 <li class="nav-item {{ Request::is('admin/categories*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('admin.categories.index') }}">
-        <i class="nav-icon icon-cursor"></i>
+        <i class="nav-icon fa fa-folder"></i>
         <span>Categories</span>
     </a>
 </li>
 <li class="nav-item {{ Request::is('admin/products*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('admin.products.index') }}">
-        <i class="nav-icon icon-cursor"></i>
+        <i class="nav-icon fa fa-barcode"></i>
         <span>Products</span>
     </a>
 </li>
 <li class="nav-item {{ Request::is('admin/carts*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('admin.carts.index') }}">
-        <i class="nav-icon icon-cursor"></i>
+        <i class="nav-icon fa fa-shopping-basket"></i>
         <span>Carts</span>
     </a>
 </li>
 <li class="nav-item {{ Request::is('admin/users*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('admin.users.index') }}">
-        <i class="nav-icon icon-cursor"></i>
+        <i class="nav-icon icon-user"></i>
         <span>Users</span>
     </a>
 </li>
