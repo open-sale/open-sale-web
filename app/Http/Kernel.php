1807356db52a4ec30526d417d0667e3ec5f67739
muathye <muath.ye@gmail.com>
2022-01-29 09:40:12 +0300

    Register json debugbar

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,10 +42,12 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            
+
             EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            \App\Http\Middleware\ProfileJsonResponse::class,
         ],
     ];
 
