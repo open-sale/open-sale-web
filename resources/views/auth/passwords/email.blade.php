85c57bdf61ac6aa2b6b490b60d781b9a8523e68f
muathye <muath.ye@gmail.com>
2022-01-30 00:08:54 +0300

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -21,6 +21,9 @@
 </head>
 <body class="app flex-row align-items-center">
 <div class="container">
+    <div class="row justify-content-center p-4">
+        <img src="{{uploads_path('open-sale.svg')}}" alt="Open Sale" width="256px">
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="card-group">
@@ -31,7 +34,7 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                        <form method="post" action="{{ url('/password/email') }}">
+                        <form method="post" action="{{ url('/admin/password/email') }}">
                             @csrf
                             <h1>Reset Your Password</h1>
                             <p class="text-muted">Enter Email to reset password</p>
