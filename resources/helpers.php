522c505723a4db65f6a66fce7c6bb20d3d1bdaba
muathye <muath.ye@gmail.com>
2022-01-29 09:42:23 +0300

    Update helpers

--- resources/helpers.php
+++ resources/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Response as Response;
+use App\Exceptions\ApiException;
 
 if (!function_exists('uploads_path')) {
     /**
@@ -14,28 +14,70 @@ if (!function_exists('uploads_path')) {
     }
 }
 
-if (!function_exists('json_success')) {
+if (! function_exists('json_success')) {
     /**
-     * Get success json response
+     * Create a new success JSON response instance.
      *
+     * @param  mixed  $data
+     * @param  mixed  $message
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  int  $options
      * @return \Illuminate\Http\JsonResponse
      */
-    function json_success($data = [], $status = Response::HTTP_OK)
+    function json_success($data, $message = 'Request done successfully.', $status = 200, array $headers = [], $options = 0)
     {
-        return response()->json($data, $status);
-        // return response()->json(['success' => true, 'data' => $data]);
+        $response = [
+            'success' => true,
+            'data'    => isset($data['data']) ? $data['data'] : $data,
+        ];
+
+        if (isset($data['pagination'])) {
+            $response['pagination'] = $data['pagination'];
+        }
+        $response['message'] = $message;
+
+
+        return response()->json($response, $status, $headers, $options)
+                ->header('X-Date', 'Tue, 28 Dec. 2021');
     }
 }
 
-if (!function_exists('json_error')) {
+if (! function_exists('json_error')) {
     /**
-     * Get success json response
+     * Create a new error JSON response instance.
      *
+     * @param  mixed  $errors
+     * @param  mixed  $message
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  int  $options
      * @return \Illuminate\Http\JsonResponse
      */
-    function json_error($data = [], $status = Response::HTTP_NOT_ACCEPTABLE)
+    function json_error($message, $errors, $status = 400, array $headers = [], $options = 0)
     {
-        return response()->json($data, $status);
-        // return response()->json(['success' => true, 'data' => $data]);
+        $response = [
+            'success' => false,
+        ];
+
+        $response['errors'] = $errors;
+
+        $response['message'] = $message;
+
+        return response()->json($response, $status, $headers, $options)
+                ->header('X-Date', 'Tue, 28 Dec. 2021');
+    }
+}
+
+if (! function_exists('jdd')) {
+    /**
+     * Create a new die and dump JSON response instance.
+     *
+     * @param  mixed  $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function jdd(...$data)
+    {
+        throw new ApiException($data, 'Die and Dump', 418);
     }
 }
