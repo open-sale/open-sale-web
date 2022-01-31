37e38072bc49af67dc8b336085d5e8805c7c061b
muathye <muath.ye@gmail.com>
2022-01-31 16:42:50 +0300

    Update app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css">
+    <link rel="stylesheet" href="/app/css/admin.min.css">
 </head>
 <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
 <header class="app-header navbar">
