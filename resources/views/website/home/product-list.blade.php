53d25934fdc31bbfb50f663adf5dd5e933687ad2
muathye <muath.ye@gmail.com>
2022-02-06 21:58:15 +0300

    Add home product-list.blade.php

---
+++ resources/views/website/home/product-list.blade.php
@@ -0,0 +1,8 @@
+<div class="mt-16">
+    <h3 class="text-gray-600 text-2xl font-medium">Fashions</h3>
+    <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+        @foreach ($products as $product)
+        @include('website.home.product-item', ['product' => $product])
+        @endforeach
+    </div>
+</div>
