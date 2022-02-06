16a944afc3309194051430d9d04bde5daef25321
muathye <muath.ye@gmail.com>
2022-02-06 22:02:00 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+// mix.js('resources/js/app.js', 'public/js')
+//     .react()
+//     .extract(["react"])
+//     .sass('resources/sass/app.scss', 'public/css');
+
+mix.js("resources/js/app.js", "public/js")
     .react()
     .extract(["react"])
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .postCss("resources/css/app.css", "public/css", [
+        require("tailwindcss"),
+    ]);
