e9c602231e5ffa1f532de1f04beaf38c6152a267
muathye <muath.ye@gmail.com>
2022-02-06 21:57:53 +0300

    Add home intro.blade.php

---
+++ resources/views/website/home/intro.blade.php
@@ -0,0 +1,18 @@
+<div class="h-64 rounded-md overflow-hidden bg-cover bg-center"
+    style="background-image: url('https://images.unsplash.com/photo-1577655197620-704858b270ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=144')">
+    <div class="bg-gray-900 bg-opacity-50 flex items-center h-full">
+        <div class="px-10 max-w-xl">
+            <h2 class="text-2xl text-white font-semibold">Sport Shoes</h2>
+            <p class="mt-2 text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Tempore facere provident molestias ipsam sint voluptatum pariatur.</p>
+            <button
+                class="flex items-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                <span>Shop Now</span>
+                <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    viewBox="0 0 24 24" stroke="currentColor">
+                    <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                </svg>
+            </button>
+        </div>
+    </div>
+</div>
